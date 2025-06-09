8fd019d7f2c6b0644a5cc12f4a49aa04829263c4
Hasan-Rza Iqbal <hasaiqba@cisco.com>
2025-06-09 16:47:35 +0100

    modifed greet_user, added location

--- git.py
+++ git.py
@@ -1,7 +1,8 @@
 def greet_user():
     name = input ("what is your name?")
     age = input ("how old are you?")
-    print (f"Hello, {name}! You are {age} years old! Welcome to the Git practice")
+    location = input ("where are you from?")
+    print (f"Hello, {name} from {location}! You are {age} years old! Welcome to the Git practice")
 
 if __name__ == "__main__":
     greet_user()
