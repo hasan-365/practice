177482f2d77743b5ddae9ca0ffc3698a3bb97e43
Hasan-Rza Iqbal <hasaiqba@cisco.com>
2025-06-10 11:04:23 +0100

    modified greet_user, added job_role

--- git.py
+++ git.py
@@ -2,7 +2,8 @@ def greet_user():
     name = input ("what is your name?")
     age = input ("how old are you?")
     location = input ("where are you from?")
-    print (f"Hello, {name} from {location}! You are {age} years old! Welcome to the Git practice")
+    job_role = input ("what is your job role?")
+    print (f"Hello, {name} an {job_role} from {location}! You are {age} years old! Welcome to the Git practice")
 
 if __name__ == "__main__":
     greet_user()
